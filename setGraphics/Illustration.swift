410e158a5782f8140d4689142b1db7e14a649326
Maulik Sharma <mauliksharma@Mauliks-MacBook.local>
2018-10-04 15:10:21 +0530

    Created squiggle

--- setGraphics/Illustration.swift
+++ setGraphics/Illustration.swift
@@ -26,7 +26,20 @@ class Illustration: UIView {
         case 2:
             path = UIBezierPath(roundedRect: bounds, cornerRadius: ovalCornerRadius)
         case 3:
-            path.move(to: <#T##CGPoint#>)
+            path.move(to: pointRelativeToBound(xToWidth: 0.05, yToHeight: 0.40))
+            
+            path.addCurve(to: pointRelativeToBound(xToWidth: 0.35, yToHeight: 0.25), controlPoint1: pointRelativeToBound(xToWidth: 0.09, yToHeight: 0.15), controlPoint2: pointRelativeToBound(xToWidth: 0.18, yToHeight: 0.10))
+            
+            path.addCurve(to: pointRelativeToBound(xToWidth: 0.77, yToHeight: 0.27), controlPoint1: pointRelativeToBound(xToWidth: 0.40, yToHeight: 0.30), controlPoint2: pointRelativeToBound(xToWidth: 0.60, yToHeight: 0.45))
+            
+            path.addCurve(to: pointRelativeToBound(xToWidth: 0.97, yToHeight: 0.35), controlPoint1: pointRelativeToBound(xToWidth: 0.87, yToHeight: 0.15), controlPoint2: pointRelativeToBound(xToWidth: 0.98, yToHeight: 0.00))
+            
+            path.addCurve(to: pointRelativeToBound(xToWidth: 0.45, yToHeight: 0.86), controlPoint1: pointRelativeToBound(xToWidth: 0.95, yToHeight: 1.10), controlPoint2: pointRelativeToBound(xToWidth: 0.50, yToHeight: 0.95))
+            
+            path.addCurve(to: pointRelativeToBound(xToWidth: 0.25, yToHeight: 0.85), controlPoint1: pointRelativeToBound(xToWidth: 0.40, yToHeight: 0.80), controlPoint2: pointRelativeToBound(xToWidth: 0.35, yToHeight: 0.75))
+            
+            path.addCurve(to: pointRelativeToBound(xToWidth: 0.05, yToHeight: 0.40), controlPoint1: pointRelativeToBound(xToWidth: 0.00, yToHeight: 1.10), controlPoint2: pointRelativeToBound(xToWidth: 0.005, yToHeight: 0.60))
+            path.close()
         default:
             break
         }
@@ -93,5 +106,12 @@ extension Illustration {
     var stripeWidth: CGFloat {
         return pathLineWidth / 5
     }
+    
+    func pointRelativeToBound(xToWidth: CGFloat, yToHeight: CGFloat) -> CGPoint {
+        let x = bounds.origin.x + (bounds.size.width * xToWidth)
+        let y = bounds.origin.y + (bounds.size.height * yToHeight)
+        return CGPoint(x: x, y: y)
+    }
+
 }
 
